0d06fa571a6cecb750c75121ff8b2f00a5150720
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2020-04-11 07:07:21 +0530

    Updated api call with sample code

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -8,11 +8,31 @@ export default class Recipe {
     async getRecipe(){
         try {
            const res = await axios(`${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
-           this.title = res.data.recipe.title;
-           this.author = res.data.recipe.publisher;
-           this.img = res.data.recipe.image_url;
-           this.url = res.data.recipe.source_url;
-           this.ingredients = res.data.recipe.ingredients;
+           const data = {
+            "recipe": {
+              "publisher": "Real Simple",
+              "f2f_url": "http://food2fork.com/view/37859",
+              "ingredients": [
+                "1 1/2 cups shredded rotisserie chicken",
+                "1 1/2 cups grated Gruyre",
+                "1 cup frozen peas",
+                "2 sheets (one 17.25-ounce package) frozen puff pastry, thawed",
+                "1 large egg, beaten",
+                "1/4 cup Dijon mustard\n"
+              ],
+              "source_url": "http://www.realsimple.com/food-recipes/browse-all-recipes/chicken-and-gruyere-turnovers-00000000052482/index.html",
+              "recipe_id": "37859",
+              "image_url": "http://static.food2fork.com/chickenturnover2_300e6667e66.jpg",
+              "social_rank": 99.84842829206659,
+              "publisher_url": "http://realsimple.com",
+              "title": "Chicken and Gruyre Turnovers"
+            }
+          }
+           this.title = data.recipe.title;
+           this.author = data.recipe.publisher;
+           this.img = data.recipe.image_url;
+           this.url = data.recipe.source_url;
+           this.ingredients = data.recipe.ingredients;
            //console.log(this.ingredients);
         } catch (error) {
             console.log(error);
